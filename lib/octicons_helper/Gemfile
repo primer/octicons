eb667206c3eeba752722ca2293b4f6e0d09ade81
Jon Rohan <rohan@github.com>
2016-06-16 16:50:48 -0400

    Adding railtie to load helper

--- lib/octicons_helper/Gemfile
+++ lib/octicons_helper/Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gemspec
 
 gem 'octicons'
+gem 'rails'
 
 group :development, :test do
   gem 'rake'
--- lib/octicons_helper/lib/octicons_helper.rb
+++ lib/octicons_helper/lib/octicons_helper.rb
@@ -1,7 +1,3 @@
 require "octicons_helper/version"
 require "octicons_helper/helper"
-require "active_support"
-
-ActiveSupport.on_load(:action_view) do
-  include OcticonsHelper
-end
+require "octicons_helper/railtie" if defined?(Rails)
