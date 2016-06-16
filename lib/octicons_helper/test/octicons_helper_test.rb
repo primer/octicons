765b6dcb59c69c73485182966c965cad0307597b
Jon Rohan <rohan@github.com>
2016-06-16 16:07:13 -0400

    Actual real tests

--- lib/octicons_helper/test/octicons_helper_test.rb
+++ lib/octicons_helper/test/octicons_helper_test.rb
@@ -1,7 +1,15 @@
 require_relative "./helper"
 
 describe OcticonsHelper do
-  it "parses interpoaltion of variables" do
-    assert_equal "1.0.0", octicon("alert")
+  it "builds an icon with just the symbol passed in" do
+    assert_match /<svg class="octicon octicon-alert"/, octicon("alert")
+  end
+
+  it "has a path" do
+    assert_match /<path/, octicon("alert")
+  end
+
+  it "adds html attributes to output" do
+    assert_match /foo="bar"/, octicon("alert", :foo => "bar")
   end
 end
