ca827dd3a994b5da09ecd2ba462d4f2a230c3c33
Jon Rohan <rohan@github.com>
2017-10-12 15:03:31 -0700

    Disable cop

--- lib/octicons_helper/lib/octicons_helper/helper.rb
+++ lib/octicons_helper/lib/octicons_helper/helper.rb
@@ -9,6 +9,6 @@ module OcticonsHelper
     return "" if symbol.nil?
 
     icon = Octicons::Octicon.new(symbol, options)
-    content_tag(:svg, icon.path.html_safe, icon.options)
+    content_tag(:svg, icon.path.html_safe, icon.options) # rubocop:disable Rails/OutputSafety
   end
 end
