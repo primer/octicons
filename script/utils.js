a7c727a482677ca1020313338a3155533f894db1
Shawn Allen <shawn.allen@github.com>
2018-08-27 23:48:04 -0700

    clean up script/utils.js

--- script/utils.js
+++ script/utils.js
@@ -1,25 +1,13 @@
-const got = require('got')
-
-module.exports = {
-  progress: (current, total) => {
-    let percentage = Math.ceil((current * 10) / total)
-    let bar = [
-      "[", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "]",
-      ` ${Math.ceil((current * 100) / total)}%`
-    ]
-    for(let i = 1; i <= percentage; i++) {
-      bar[i] = "="
-    }
-    return bar.join("")
-  },
-
-  downloadSVG: (url, name, downloadProgress) => {
-    return new Promise(async (resolve, reject) => {
-      const response = await got.get(url, {
-        headers: { "Content-Type": "images/svg+xml" }
-      }).on('downloadProgress', downloadProgress)
-
-      resolve(response.body)
-    })
+function progress(current, total) {
+  let percentage = Math.ceil((current * 10) / total)
+  let bar = [
+    "[", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "]",
+    ` ${Math.ceil((current * 100) / total)}%`
+  ]
+  for(let i = 1; i <= percentage; i++) {
+    bar[i] = "="
   }
+  return bar.join("")
 }
+
+module.exports = {progress}
