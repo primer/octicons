3148b39a77a92d45eacde87cf480256be4c7142d
Shawn Allen <shawn.allen@github.com>
2018-08-28 09:34:26 -0700

    abstract out some stuff to utils

--- script/utils.js
+++ script/utils.js
@@ -1,3 +1,13 @@
+const execa = require('execa')
+const PQueue = require('p-queue')
+
+// this works around an issue with the TLS library that
+// (apparently) every Node fetch() implementation uses
+function fetchSSLFix(url) {
+  return execa('curl', ['-sL', url])
+    .then(res => res.stdout)
+}
+
 function progress(current, total) {
   let percentage = Math.ceil((current * 10) / total)
   let bar = [
@@ -10,4 +20,13 @@ function progress(current, total) {
   return bar.join("")
 }
 
-module.exports = {progress}
+function queueTasks(tasks, options) {
+  const queue = new PQueue(Object.assign({concurrency: 3}, options))
+  for (const task of tasks) {
+    queue.add(task)
+  }
+  queue.start()
+  return queue.onIdle()
+}
+
+module.exports = {fetchSSLFix, progress, queueTasks}
