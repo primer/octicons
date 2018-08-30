fd92766c0ea3419c6b3dd46b7be145b282411ea3
Shawn Allen <shawn.allen@github.com>
2018-08-30 11:11:37 -0700

    v8.1.0

--- lib/octicons_helper/lib/octicons_helper/version.rb
+++ lib/octicons_helper/lib/octicons_helper/version.rb
@@ -1,3 +1,3 @@
 module OcticonsHelper
-  VERSION = "8.0.1".freeze
+  VERSION = "8.1.0".freeze
 end
--- lib/octicons_jekyll/lib/jekyll-octicons/version.rb
+++ lib/octicons_jekyll/lib/jekyll-octicons/version.rb
@@ -3,6 +3,6 @@ module Liquid; class Tag; end; end
 
 module Jekyll
   class Octicons < Liquid::Tag
-    VERSION = "8.0.1".freeze
+    VERSION = "8.1.0".freeze
   end
 end
