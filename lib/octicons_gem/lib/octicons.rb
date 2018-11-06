1bb6491497d2bc29efc14da46528f1832dd837b2
Shawn Allen <shawn.allen@github.com>
2018-11-06 15:19:13 -0800

    debug JSON data

--- lib/octicons_gem/lib/octicons.rb
+++ lib/octicons_gem/lib/octicons.rb
@@ -4,5 +4,6 @@ require "json"
 
 module Octicons
   file_data = File.read(File.join(File.dirname(__FILE__), "./build/data.json"))
+  puts "Octicons::OCTICON_SYMBOLS: #{file_data}"
   OCTICON_SYMBOLS = JSON.parse(file_data).freeze
 end
