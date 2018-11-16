2edf502d28b0444363ad36e2a95a016e807a76bf
Emily Brick <emilybrick@github.com>
2018-11-16 12:29:10 -0800

    v8.1.3

--- lib/octicons_gem/lib/octicons/version.rb
+++ lib/octicons_gem/lib/octicons/version.rb
@@ -1,3 +1,3 @@
 module Octicons
-  VERSION = "8.1.2".freeze
+  VERSION = "8.1.3".freeze
 end
--- lib/octicons_jekyll/lib/jekyll-octicons/version.rb
+++ lib/octicons_jekyll/lib/jekyll-octicons/version.rb
@@ -3,6 +3,6 @@ module Liquid; class Tag; end; end
 
 module Jekyll
   class Octicons < Liquid::Tag
-    VERSION = "8.1.2".freeze
+    VERSION = "8.1.3".freeze
   end
 end
