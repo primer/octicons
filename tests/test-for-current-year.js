8aab22e71dbda4538ff8685eda1c03fa5989eb98
Shawn Allen <shawn.allen@github.com>
2018-12-19 13:03:24 -0800

    get LICENSE paths from lerna.json, lint

--- tests/test-for-current-year.js
+++ tests/test-for-current-year.js
@@ -1,17 +1,19 @@
-const test = require("ava")
-const fs = require("fs-extra")
-const globby = require("globby")
+const test = require('ava')
+const fs = require('fs-extra')
+const globby = require('globby')
+const {join} = require('path')
 
-const year = (new Date()).getFullYear()
+const year = new Date().getFullYear()
 const yearRegex = new RegExp(`Copyright \\(c\\) ${year} GitHub Inc\\.`)
+const {packages} = require('../lerna.json')
+const globs = packages.map(glob => join(glob, 'LICENSE'))
 
 test(`LICENSE files have the current year ${year}`, t => {
-  return globby(["**/LICENSE", "!**/node_modules/**/LICENSE", "!**/vendor/**/LICENSE"])
-    .then(paths => {
-      t.plan(paths.length)
-      return paths.map(path => {
-        const license = fs.readFileSync(path, "utf8")
-        return t.regex(license, yearRegex, `The license "${path}" does not include the current year ${year}`)
-      })
+  return globby(globs).then(paths => {
+    t.plan(paths.length)
+    return paths.map(path => {
+      const license = fs.readFileSync(path, 'utf8')
+      return t.regex(license, yearRegex, `The license "${path}" does not include the current year ${year}`)
     })
+  })
 })
