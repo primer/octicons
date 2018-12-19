67ffb6fb4bf59f6eeb2e5bdd6084074853019d56
Shawn Allen <shawn.allen@github.com>
2018-12-19 13:05:50 -0800

    lint, refactor

--- node/octicons/index.js
+++ node/octicons/index.js
@@ -1,63 +1,62 @@
-var data = require('./build/data.json')
-var objectAssign = require('object-assign')
+const icons = require('./build/data.json')
 
-Object.keys(data).forEach(function(key) {
+for (const key of Object.keys(icons)) {
+  const icon = icons[key]
+
+  // Set the symbol for easy access
+  icon.symbol = key
+
+  // Set all the default options
+  icon.options = {
+    version: '1.1',
+    width: icon.width,
+    height: icon.height,
+    viewBox: `0 0 ${icon.width} ${icon.height}`,
+    class: `octicon octicon-${key}`,
+    'aria-hidden': 'true'
+  }
+
+  // Function to return an SVG object
+  icon.toSVG = options => {
+    return `<svg ${icon.getAttributes(options)}>${icon.path}</svg>`
+  }
 
   // Returns a string representation of html attributes
-  var htmlAttributes = function(icon, options) {
-    var attributes = []
-    var attrObj = objectAssign({}, data[key].options, options)
+  icon.getAttributes = options => {
+    const attributes = []
+    const attrObj = Object.assign({}, icon.options, options)
 
     // If the user passed in options
     if (options) {
-
+      const {width, height} = options
       // If any of the width or height is passed in
-      if(options["width"] || options["height"]) {
-        attrObj["width"] = options["width"] ? options["width"] : (parseInt(options["height"]) * data[key].options["width"] / data[key].options["height"])
-        attrObj["height"] = options["height"] ? options["height"] : (parseInt(options["width"]) * data[key].options["height"] / data[key].options["width"])
+      if (width || height) {
+        attrObj.width = width ? width : (parseInt(height) * icon.options.width) / icon.options.height
+        attrObj.height = height ? height : (parseInt(width) * icon.options.height) / icon.options.width
       }
 
       // If the user passed in class
-      if (options["class"]) {
-        attrObj["class"] = "octicon octicon-" + key + " " + options["class"]
-        attrObj["class"].trim()
+      if (options['class']) {
+        attrObj['class'] = `octicon octicon-${key} ${options['class']}`
+        attrObj['class'].trim()
       }
 
       // If the user passed in aria-label
-      if (options["aria-label"]) {
-        attrObj["aria-label"] = options["aria-label"]
-        attrObj["role"] = "img"
+      if (options['aria-label']) {
+        attrObj['aria-label'] = options['aria-label']
+        attrObj.role = 'img'
 
         // Un-hide the icon
-        delete attrObj["aria-hidden"]
+        delete attrObj['aria-hidden']
       }
     }
 
     Object.keys(attrObj).forEach(function(option) {
-      attributes.push(option + "=\"" + attrObj[option] + "\"")
+      attributes.push(`${option}="${attrObj[option]}"`)
     })
 
-    return attributes.join(" ").trim()
+    return attributes.join(' ').trim()
   }
+}
 
-  // Set the symbol for easy access
-  data[key].symbol = key
-
-  // Set all the default options
-  data[key].options = {
-    "version": "1.1",
-    "width": data[key].width,
-    "height": data[key].height,
-    "viewBox": "0 0 " + data[key].width + " " + data[key].height,
-    "class": "octicon octicon-" + key,
-    "aria-hidden": "true"
-  }
-
-  // Function to return an SVG object
-  data[key].toSVG = function(options) {
-    return "<svg " + htmlAttributes(data[key], options) + ">" + data[key].path + "</svg>"
-  }
-})
-
-// Import data into exports
-module.exports = data
+module.exports = icons
