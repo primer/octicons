3d18bc2ce24978b330b13beeb6210b45515dc531
Jon Rohan <yes@jonrohan.codes>
2019-01-28 15:05:19 -0800

    copy utils.js

---
+++ .github/actions/figma-asset-action/utils.js
@@ -0,0 +1,43 @@
+const PQueue = require('p-queue')
+const execa = require('execa')
+const yaml = require('js-yaml')
+const {readFileSync} = require('fs')
+
+// this works around an issue with the TLS library that
+// (apparently) every Node fetch() implementation uses
+function fetchSSLFix(url) {
+  return execa('curl', ['-sL', url]).then(res => res.stdout)
+}
+
+function loadYAML(path) {
+  return yaml.safeLoad(readFileSync(path, 'utf8'))
+}
+
+function progress(current, total) {
+  let percentage = Math.ceil((current * 10) / total)
+  if (!Number.isFinite(percentage)) {
+    console.warn(`ack! infinite % for (${current} * 10 / ${total})`)
+    percentage = 0
+  }
+  const bar = ['[', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ']', ` ${Math.ceil((current * 100) / total)}%`]
+  for (let i = 1; i <= percentage; i++) {
+    bar[i] = '='
+  }
+  return bar.join('')
+}
+
+function queueTasks(tasks, options) {
+  const queue = new PQueue(Object.assign({concurrency: 3}, options))
+  for (const task of tasks) {
+    queue.add(task)
+  }
+  queue.start()
+  return queue.onIdle()
+}
+
+module.exports = {
+  fetchSSLFix,
+  loadYAML,
+  progress,
+  queueTasks
+}
