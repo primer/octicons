ee0b137e139416a3e34259345399589b6016ac71
Emily <emplums@github.com>
2019-01-29 14:10:17 -0800

    remove list

--- .github/actions/bundle/entrypoint.sh
+++ .github/actions/bundle/entrypoint.sh
@@ -10,12 +10,8 @@ cp -R ../build ./lib
 echo "################## Bundle installing"
 bundle install
 
-echo "######## list"
-ls -la lib
-
 echo "################## Linting"
 bundle exec rubocop
-
 
 echo "################## Running tests"
 bundle exec rake
