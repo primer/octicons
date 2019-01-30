39f6fb9db282e8a2e7fea559ef3444c420098d49
Jon Rohan <yes@jonrohan.codes>
2019-01-30 15:28:51 -0800

    Fix test linting

--- tests/test-for-current-year.js
+++ tests/test-for-current-year.js
@@ -1,17 +1,16 @@
-const test = require("ava")
-const fs = require("fs-extra")
-const globby = require("globby")
+const test = require('ava')
+const fs = require('fs-extra')
+const globby = require('globby')
 
-const year = (new Date()).getFullYear()
+const year = new Date().getFullYear()
 const yearRegex = new RegExp(`Copyright \\(c\\) ${year} GitHub Inc\\.`)
 
 test(`LICENSE files have the current year ${year}`, t => {
-  return globby(["**/LICENSE", "!**/node_modules/**/LICENSE", "!**/vendor/**/LICENSE"])
-    .then(paths => {
-      t.plan(paths.length)
-      return paths.map(path => {
-        const license = fs.readFileSync(path, "utf8")
-        return t.regex(license, yearRegex, `The license "${path}" does not include the current year ${year}`)
-      })
+  return globby(['**/LICENSE', '!**/node_modules/**/LICENSE', '!**/vendor/**/LICENSE']).then(paths => {
+    t.plan(paths.length)
+    return paths.map(path => {
+      const license = fs.readFileSync(path, 'utf8')
+      return t.regex(license, yearRegex, `The license "${path}" does not include the current year ${year}`)
     })
+  })
 })
