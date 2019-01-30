d0550ee7640961b2bdef225f303f3d911560dea7
Jon Rohan <yes@jonrohan.codes>
2019-01-29 21:57:31 -0800

    lint

--- tests/index.js
+++ tests/index.js
@@ -1,8 +1,7 @@
-import test from 'ava';
-import fs from 'fs';
+import test from 'ava'
+import fs from 'fs'
 
-const octiconsLib = fs.readdirSync("./lib/build/svg");
-
+const octiconsLib = fs.readdirSync('./lib/build/svg')
 
 test('SVG icons exist', t => {
   t.not(octiconsLib.length, 0, `We didn't find any svg files`)
