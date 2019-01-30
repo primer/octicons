21266e862f9f0caa29168b617a5fa7285cfa7795
Jon Rohan <yes@jonrohan.codes>
2019-01-30 14:35:21 -0800

    write

--- .github/actions/version/entrypoint.js
+++ .github/actions/version/entrypoint.js
@@ -1,14 +1,32 @@
 const semver = require('semver')
+const fs = require('fs')
 const {
   join,
   resolve
 } = require('path')
+
 const {
-  GITHUB_SHA,
-  GITHUB_REF
+  GITHUB_REF,
+  GITHUB_SHA
 } = process.env
-
 const pkg = require(resolve(process.cwd(), 'package.json'))
+const branchName = GITHUB_REF.split('/').pop()
 
-console.log(pkg.version)
-console.log(GITHUB_REF, GITHUB_SHA)
+const writePackageJson = () => {
+  fs.writeFileSync(join(process.cwd(), 'package.json'), JSON.stringify(pkg), 'utf8')
+}
+
+// If it's a release branch
+if (branchName.match(/^release-/i)) {
+  console.log('Versioning release candidate')
+  const newVersion = branchName.match(/^release-([\d\.]+)/i)
+  pkg.version = `${newVersion[1]}-rc.${GITHUB_SHA.slice(0,7)}`
+  writePackageJson()
+}
+
+// Otherwise
+else if (branchName != 'master') {
+  console.log('Versioning prerelease')
+  pkg.version = `${semver.inc(pkg.version, 'patch')}-alpha.${GITHUB_SHA.slice(0,7)}`
+  writePackageJson()
+}
