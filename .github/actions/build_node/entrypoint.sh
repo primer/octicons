c8cfe32086c28c610154475264988308a1f2c8f0
Jon Rohan <yes@jonrohan.codes>
2019-01-31 12:59:58 -0800

    npmrc

--- .github/actions/build_node/entrypoint.sh
+++ .github/actions/build_node/entrypoint.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+if [ -n "$NPM_AUTH_TOKEN" ]; then
+  # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
+  NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG-"$HOME/.npmrc"}"
+  NPM_REGISTRY_URL="${NPM_REGISTRY_URL-registry.npmjs.org}"
+
+  # Allow registry.npmjs.org to be overridden with an environment variable
+  printf "//%s/:_authToken=%s\\nregistry=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
+  chmod 0600 "$NPM_CONFIG_USERCONFIG"
+fi
+
 PACKAGE_VERSION=$(cat package.json \
   | grep version \
   | head -1 \
