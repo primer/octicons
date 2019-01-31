9776a3ed6965e1eb77394c0eabee4fbf04571457
Jon Rohan <yes@jonrohan.codes>
2019-01-31 15:42:34 -0800

    Versioning and publishing

--- .github/actions/build_ruby/entrypoint.sh
+++ .github/actions/build_ruby/entrypoint.sh
@@ -1,6 +1,17 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
 set -e
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+# Setup rubygems creds
+echo "---\n:rubygems_api_key: $RUBYGEMS_TOKEN" > ~/.gem/credentials
+chmod 0600 ~/.gem/credentials
 
 cd ./lib/$*
 
@@ -10,11 +21,14 @@ cp -R ../build lib/
 echo "**************** Installing ****************"
 bundle install
 
-echo "**************** Building ****************"
-# bundle install
-
 echo "**************** Linting ****************"
 bundle exec rake lint
 
 echo "**************** Testing  ****************"
 bundle exec rake test
+
+echo "**************** Versioning ****************"
+bundle exec rake version\["$PACKAGE_VERSION"\]
+
+echo "**************** Building ****************"
+(bundle exec rake build && bundle exec rake release\[remote\]) && wait
