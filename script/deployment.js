9592a24d128d700a533e58e8fa2bce05a1599e0c
Jon Rohan <yes@jonrohan.codes>
2019-02-12 16:01:21 -0800

    more details

--- script/deployment.js
+++ script/deployment.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const Octokit = require('@octokit/rest')
+const pkg = require('package.json')
 
 const octokit = new Octokit({
   auth: `token ${process.env.GITHUB_TOKEN}`
@@ -12,8 +13,10 @@ octokit.repos
     owner: process.env.GITHUB_REPOSITORY.split('/')[0],
     repo: process.env.GITHUB_REPOSITORY.split('/')[1],
     ref: process.env.GITHUB_REF,
-    required_contexts: []
+    required_contexts: [],
+    environment: `npm`,
+    description: `${pkg.name}@${pkg.version}`
   })
   .then(result => {
-    console.log(result)
+    console.log(result.data)
   })
