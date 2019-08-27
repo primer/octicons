7ca6f139594b4d272148f9888b5d7107d8690dd0
Max Stoiber <mxstbr@github.com>
2019-08-27 09:51:23 +0200

    Wrap each icon in <Octicon /> directly

--- .svgrrc.js
+++ .svgrrc.js
@@ -1,16 +1,22 @@
 module.exports = {
   template: (babel, opts, {imports, componentName, props, jsx, exports}) => {
-    // Replace the <svg></svg> wrapper with a fragment (we need the fragment in case there are multiple paths).
+    // Replace the <svg></svg> wrapper with <Octicon />
     // For example:
-    // <svg some="attr"><path /><path /></svg> => <React.Fragment><path /><path /></React.Fragment>
-    const pathJsx = babel.types.jsxFragment(
-      babel.types.jsxOpeningFragment(),
-      babel.types.jsxClosingFragment(),
+    // <svg some="attr"><path /></svg> => <Octicon><path /></Octicon>
+    const pathJsx = babel.types.jsxElement(
+      babel.types.jsxOpeningElement(babel.types.jsxIdentifier('Octicon'), [
+        babel.types.jsxSpreadAttribute(babel.types.identifier('props'))
+      ]),
+      babel.types.jsxClosingElement(babel.types.jsxIdentifier('Octicon')),
       jsx.children
     )
+
     return babel.template.ast`
       ${imports}
+      import Octicon from '@primer/octicons-react'
+
       const ${componentName} = (${props}) => ${pathJsx}
+
       ${exports}
     `
   }
