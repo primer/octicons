be2ea3be9a31cfe4bd419773b4c293fe4d7bb54d
Cole Bemis <colebemis@github.com>
2020-04-22 12:50:29 -0700

    Remove iconsByName and getIconByName for real

--- lib/octicons_react/ts-tests/index.tsx
+++ lib/octicons_react/ts-tests/index.tsx
@@ -1,35 +1,5 @@
 import * as React from 'react'
-// import Octicon, {getIconByName, iconsByName, MarkGithubIcon, OcticonProps, PlusIcon, RepoIcon} from '../src'
-import Octicon, {MarkGithubIcon, OcticonProps, PlusIcon, RepoIcon} from '../src'
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
-
-// function OcticonByName({name, ...props}: {name: keyof iconsByName} & Omit<OcticonProps, 'icon'>): React.ReactElement {
-//   return <Octicon {...props} icon={getIconByName(name)} />
-// }
-
-// function TestOcticonsByName(): React.ReactElement {
-//   return <OcticonByName name="x" />
-// }
-
-// Unfortunately, `Object.keys` returns `string[]` unconditionally;
-// see https://github.com/Microsoft/TypeScript/pull/13971 &
-// https://github.com/Microsoft/TypeScript/issues/12870 for details.
-function keys<T>(obj: T): (keyof T)[] {
-  return Object.keys(obj) as (keyof T)[]
-}
-
-// function OcticonsList() {
-//   return (
-//     <ul>
-//       {keys(iconsByName).map(key => (
-//         <li key={key}>
-//           <code>{key}</code>
-//           <Octicon icon={iconsByName[key]} />
-//         </li>
-//       ))}
-//     </ul>
-//   )
-// }
+import Octicon, {MarkGithubIcon, PlusIcon, RepoIcon} from '../src'
 
 function TestOcticons() {
   return (
