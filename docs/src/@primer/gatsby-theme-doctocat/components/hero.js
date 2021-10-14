7d0b15e3f045e227eb71a712f3d33c289fdb74f5
Cole Bemis <colebemis@github.com>
2021-10-14 14:45:57 -0700

    Update color variables

--- docs/src/@primer/gatsby-theme-doctocat/components/hero.js
+++ docs/src/@primer/gatsby-theme-doctocat/components/hero.js
@@ -1,23 +1,25 @@
-import {Box, Heading, Text} from '@primer/components'
+import {Box, Heading, Text, ThemeProvider} from '@primer/components'
 import {Container} from '@primer/gatsby-theme-doctocat'
 import React from 'react'
 import {version} from '../../../../../package.json'
 
 function Hero() {
   return (
-    <Box bg="black" py={6}>
-      <Container>
-        <Heading color="blue.4" fontSize={7} m={0}>
-          Octicons
-        </Heading>
-        <Text as="p" mt={0} mb={3} color="blue.2" fontSize={4}>
-          Your project. GitHub's icons.
-        </Text>
-        <Text as="p" m={0} color="blue.3" fontFamily="mono">
-          v{version}
-        </Text>
-      </Container>
-    </Box>
+    <ThemeProvider colorMode="night" nightScheme="dark_dimmed">
+      <Box bg="canvas.default" py={6}>
+        <Container>
+          <Heading color="accent.fg" fontSize={7} m={0}>
+            Octicons
+          </Heading>
+          <Text as="p" mt={0} mb={3} color="fg.default" fontSize={4}>
+            Your project. GitHub's icons.
+          </Text>
+          <Text as="p" m={0} color="fg.default" fontFamily="mono">
+            v{version}
+          </Text>
+        </Container>
+      </Box>
+    </ThemeProvider>
   )
 }
 
