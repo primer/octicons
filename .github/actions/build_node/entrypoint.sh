a81f12a239672feff419da0d9a03738181a39f3a
Armagan Ersoz <broccolinisoup@github.com>
2023-05-11 08:18:07 +1000

    try

--- .github/actions/build_node/entrypoint.sh
+++ .github/actions/build_node/entrypoint.sh
@@ -5,10 +5,12 @@ set -e
 if [ -n "$NPM_AUTH_TOKEN" ]; then
   # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
   NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG-"$HOME/.npmrc"}"
-  NPM_REGISTRY_URL="${NPM_REGISTRY_URL-registry.npmjs.org}"
+  NPM_REGISTRY_URL="${NPM_REGISTRY_URL-https://registry.npmjs.org}"
 
   # Allow registry.npmjs.org to be overridden with an environment variable
-  printf "https://%s/:_authToken=%s\\nregistry=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
+  echo "$NPM_REGISTRY_URL"
+  echo "//%s/:_authToken=%s\\nregistry=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "$NPM_REGISTRY_URL"
+  printf "//%s/:_authToken=%s\\nregistry=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
   chmod 0600 "$NPM_CONFIG_USERCONFIG"
 fi
 
