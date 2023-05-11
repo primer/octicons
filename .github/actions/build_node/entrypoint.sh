c37b9f2bd974d75b15f289ac5e5e044af4ce4ad0
Jon Rohan <rohan@github.com>
2023-05-10 18:15:14 -0700

    Change to $NPM_AUTH_TOKEN_SHARED

--- .github/actions/build_node/entrypoint.sh
+++ .github/actions/build_node/entrypoint.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-if [ -n "$NPM_AUTH_TOKEN" ]; then
+if [ -n "$NPM_AUTH_TOKEN_SHARED" ]; then
   # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
   NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG-"$HOME/.npmrc"}"
   NPM_REGISTRY_URL="${NPM_REGISTRY_URL-https://registry.npmjs.org}"
 
   # Allow registry.npmjs.org to be overridden with an environment variable
-  printf "//%s/:_authToken=%s\\nregistry=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
+  printf "//%s/:_authToken=%s\\nregistry=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN_SHARED" "$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
   chmod 0600 "$NPM_CONFIG_USERCONFIG"
 fi
 
