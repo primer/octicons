cf22546862c61631e9f41105bfeab6e877476049
Benjamin Bädorf <hello@benjaminbaedorf.eu>
2023-07-24 12:02:07 +0200

    fix: don't use outerHTML, add specs to Angular package

---
+++ lib/octicons_angular/src/public-api.spec.ts
@@ -0,0 +1,59 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import {
+  OpPlusIcon,
+  OpOpAddIcon,
+} from './public-api';
+
+describe('Github native icon', () => {
+  let component: OpPlusIcon;
+  let fixture: ComponentFixture<OpPlusIcon>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({imports: [OpPlusIcon]}).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OpPlusIcon);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeDefined();
+  });
+});
+
+describe('OpenProject extension icon', () => {
+  let component: OpOpAddIcon;
+  let fixture: ComponentFixture<OpOpAddIcon>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({imports: [OpOpAddIcon]}).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OpOpAddIcon);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeDefined();
+  });
+
+  it('should render the svg', () => {
+    const iconElement: HTMLElement = fixture.nativeElement;
+    expect(iconElement.children[0].children[0].tagName.toLowerCase()).toEqual("path");
+  });
+
+  it('should render the title', () => {
+    const iconElement: HTMLElement = fixture.nativeElement;
+    expect(iconElement.children[0].children[0].tagName.toLowerCase()).toEqual("path");
+
+    component.title = "Some title";
+    fixture.detectChanges();
+
+    expect(iconElement.children[0].tagName.toLowerCase()).toEqual("title");
+    expect(iconElement.children[1].children[0].tagName.toLowerCase()).toEqual("path");
+  });
+});
