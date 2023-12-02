738cab450e9b7c4190e535894a1acfe9f0b34609
anosim114 <arnold_siemens@pm.me>
2023-12-02 18:35:51 +0100

    fix yarn tests

--- tests/index.js
+++ tests/index.js
@@ -7,8 +7,7 @@ const globby = require('globby')
 
 const year = new Date().getFullYear()
 const yearRegex = new RegExp(`Copyright \\(c\\) ${year} GitHub Inc\\.`)
-const octiconsLib = fs.readdirSync('./lib/build/svg')
-const octiconsData = require('../lib/build/data.json')
+const forticonsData = require('../lib/build/data.json')
 
 test(`LICENSE files have the current year ${year}`, t => {
   return globby(['**/LICENSE', '!**/node_modules/**/LICENSE', '!**/vendor/**/LICENSE']).then(paths => {
@@ -20,23 +19,19 @@ test(`LICENSE files have the current year ${year}`, t => {
   })
 })
 
-test('SVG icons exist', t => {
-  t.not(octiconsLib.length, 0, `We didn't find any svg files`)
-})
-
 test('Data file exist', t => {
-  t.not(octiconsData.length, 0, `We didn't find any data files`)
+  t.not(forticonsData.length, 0, `We didn't find any data files`)
 })
 
 const names = {}
-for (const octicon of Object.values(octiconsData)) {
-  test(`No duplicate ${octicon.name} icon`, t => {
-    if (names[octicon.name]) {
+for (const forticon of Object.values(forticonsData)) {
+  test(`No duplicate ${forticon.name} icon`, t => {
+    if (names[forticon.name]) {
       t.fail(
-        `Found duplicate '${octicon.name}' icons in the figma file. Please rename one of them. https://www.figma.com/file/${octicon.file}?node-id=${octicon.id}`
+        `Found duplicate '${forticon.name}' icons in the figma file. Please rename one of them. https://www.figma.com/file/${forticon.file}?node-id=${forticon.id}`
       )
     } else {
-      names[octicon.name] = octicon
+      names[forticon.name] = forticon
       t.pass()
     }
   })
