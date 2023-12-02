d275060b08000feadd19366e04b044057093eca8
anosim114 <arnold_siemens@pm.me>
2023-12-02 19:33:27 +0100

    Please please let the tests be fixed

--- lib/forticons_gem/test/forticons_test.rb
+++ lib/forticons_gem/test/forticons_test.rb
@@ -3,12 +3,10 @@ require_relative "./helper"
 describe Forticons do
   it "loads all icons on initialization" do
     refute_equal 0, Forticons::FORTICON_SYMBOLS.length
-    x_icon = Forticons::FORTICON_SYMBOLS["x"]
+    x_icon = Forticons::FORTICON_SYMBOLS["suitcase"]
     assert x_icon["name"]
     assert x_icon["keywords"]
     assert x_icon["heights"]
-    assert x_icon["heights"]["16"]
-    assert x_icon["heights"]["16"]["width"]
-    assert x_icon["heights"]["16"]["path"]
+    assert x_icon["heights"]["512"]
   end
 end
