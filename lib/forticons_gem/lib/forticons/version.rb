0a2cd19160c84f032de469a390f750e0280d71ea
anosim114 <arnold_siemens@pm.me>
2023-12-02 19:45:52 +0100

    fix forticons_helper tests

--- lib/forticons_gem/lib/forticons/version.rb
+++ lib/forticons_gem/lib/forticons/version.rb
@@ -1,3 +1,3 @@
 module Forticons
-  VERSION = "0.0.1".freeze
+  VERSION = "0.0.2".freeze
 end
--- lib/forticons_helper/test/octicons_helper_test.rb
+++ lib/forticons_helper/test/octicons_helper_test.rb
@@ -7,15 +7,17 @@ describe ForticonsHelper do
     end
 
     it "renders the svg" do
-      assert_match /<svg.*forticon-x.*>.*<\/svg>/, forticon("x")
+      assert_includes forticon("x"), "<svg"
+      assert_includes forticon("x"), "forticon-x"
+      assert_includes forticon("x"), "<\/svg>"
     end
 
     it "has a path" do
-      assert_match /<path/, forticon("alert")
+      assert_match /<path/, forticon("suitcase")
     end
 
     it "adds html attributes to output" do
-      assert_match /foo="bar"/, forticon("alert", foo: "bar")
+      assert_match /foo="bar"/, forticon("suitcase", foo: "bar")
     end
 
     # it "caches SVGs for two calls with the same arguments" do
