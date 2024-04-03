78fd3f75501dec7a8b620e9d92053d8b641a0e8d
Cameron Dutro <camertron@gmail.com>
2024-04-03 15:28:43 -0700

    Make diff smaller

--- lib/octicons_gem/lib/octicons/version.rb
+++ lib/octicons_gem/lib/octicons/version.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 module Octicons
   VERSION = "19.9.0".freeze
 end
--- lib/octicons_jekyll/lib/jekyll-octicons/version.rb
+++ lib/octicons_jekyll/lib/jekyll-octicons/version.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # Prevent bundler errors
 module Liquid; class Tag; end; end
 
